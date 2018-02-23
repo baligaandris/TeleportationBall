a9fc58ffa3f0bd6f002da96f8276ee5d7fca52e3
RobertKlivis <roberts.klivis@gmail.com>
2018-02-23 18:43:39 +0000

    Added Goal Text
    Added basic goal text whenever a goal is  scored.

---
+++ Assets/GoalText.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalText : MonoBehaviour {
+
+    public string text;
+    public bool display = false;
+    GUIStyle Font;
+    public float targetTime = 1.0f;
+    bool Var;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        Font = new GUIStyle();
+
+        Font.fontSize = 32;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Var)
+        {
+            targetTime -= Time.deltaTime;
+
+            if (targetTime <= 0.0f)
+            {
+                timerEnded();
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D Ball)
+    {
+
+        display = true;
+        Var = true;
+
+    }
+
+    void timerEnded()
+    {
+
+        display = false;
+
+    }
+
+    void OnGUI()
+    {
+
+        if (display == true)
+
+        {
+
+            GUI.Label(new Rect(460, 80, 200, 80), text, Font);
+
+        }
+
+    }
+}
+
